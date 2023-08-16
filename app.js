787bd92384176647d4ee0b8ac85b0d97a6283d6f
Danyalsmit <121617130+Danyalsmit@users.noreply.github.com>
2023-08-16 11:04:39 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function abc() {
+    // var input = document.getElementById("email");
+    // input.value ="danyal khan "
+    var text = document.getElementById("text")
+    var fullText =
+        "Quibusdam  obcaecati pariatur ullam Repellendus quisquam esse sed,architecto incidunt  eius rerum expedita accusamus assumenda."
+        text.innerHTML=fullText;
+    console.log(text.innerHTML)
+
+    //     if (input.value.trim()=== "") {
+    //         alert("enter the value")
+    //     } else {
+    //         console.log(input.value)
+    //     }
+}
